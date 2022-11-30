303bbafae783aa0e0fe7675ab213f6148eab7e7c
Nicholas Ung <ung.nicholask@gmail.com>
2022-11-29 21:08:19 -0800

    Initial Commit

---
+++ baristaChallenge/src/com/nicholasung/baristaChallenge/Item.java
@@ -0,0 +1,38 @@
+package com.nicholasung.baristaChallenge;
+
+public class Item {
+	// MEMBER VARIABLES
+	private String name;
+	private double price;
+	
+	// CONSTRUCTOR
+	public Item() {
+		this.name = null;
+		this.price = 0;
+	}
+	// OVERLOADED CONSTRUCTOR
+	public Item(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	// GETTERS & SETTERS
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+
+	
+}
---
+++ baristaChallenge/src/com/nicholasung/baristaChallenge/TestOrders.java
@@ -0,0 +1,10 @@
+package com.nicholasung.baristaChallenge;
+
+public class TestOrders {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
